47304456d19b7899a35ba6b91c6a61e134c2934b
Timofey <timofeysalmin@gmail.com>
2016-07-01 22:25:39 +0300

    010716-3-22:25

--- models/UsersPurchasesModel.php
+++ models/UsersPurchasesModel.php
@@ -24,11 +24,111 @@ class UsersPurchasesModel extends AbstractBaseModel
     public $id_deliveries = '';
     public $id_payments = '';
     
+    private $_received = 0;
+    private $_received_date = NULL;
+    private $_processed = 0;
+    
     public function scenarios()
     {
         return [
             self::GET_FROM_FORM=>['id_users', 'id_products', 'id_deliveries', 'id_payments'],
-            self::GET_FROM_DB=>['id', 'id_users', 'id_products', 'id_deliveries', 'id_payments'],
+            self::GET_FROM_DB=>['id', 'id_users', 'id_products', 'id_deliveries', 'id_payments', 'received', 'received_date'],
         ];
+    }
+    
+    /**
+     * Присваивает значение свойству $this->_received
+     * @param string $value значение received
+     * @return boolean
+     */
+    public function setReceived($value)
+    {
+        try {
+            if ($value) {
+                $this->_received = 1;
+            }
+            return true;
+        } catch (\Exception $e) {
+            $this->throwException($e, __METHOD__);
+        }
+    }
+    
+    /**
+     * Возвращает значение свойства $this->_received
+     * @return int
+     */
+    public function getReceived()
+    {
+        try {
+            if ($this->scenario == UsersPurchasesModel::GET_FROM_FORM) {
+                $this->_received = 1;
+            }
+            return $this->_received;
+        } catch (\Exception $e) {
+            $this->throwException($e, __METHOD__);
+        }
+    }
+    
+    /**
+     * Присваивает значение свойству $this->_received_date
+     * @param string $value значение received_date
+     * @return boolean
+     */
+    public function setReceived_date($value)
+    {
+        try {
+            $this->_received_date = $value;
+            return true;
+        } catch (\Exception $e) {
+            $this->throwException($e, __METHOD__);
+        }
+    }
+    
+    /**
+     * Возвращает значение свойства $this->_received_date
+     * @return int
+     */
+    public function getReceived_date()
+    {
+        try {
+            if (is_null($this->_received_date)) {
+                if ($this->scenario == UsersPurchasesModel::GET_FROM_FORM) {
+                    $this->_received_date = time();
+                }
+            }
+            return $this->_received_date;
+        } catch (\Exception $e) {
+            $this->throwException($e, __METHOD__);
+        }
+    }
+    
+    /**
+     * Присваивает значение свойству $this->_processed
+     * @param string $value значение processed
+     * @return boolean
+     */
+    public function setProcessed($value)
+    {
+        try {
+            if ($value) {
+                $this->_processed = 1;
+            }
+            return true;
+        } catch (\Exception $e) {
+            $this->throwException($e, __METHOD__);
+        }
+    }
+    
+    /**
+     * Возвращает значение свойства $this->_processed
+     * @return int
+     */
+    public function getProcessed()
+    {
+        try {
+            return $this->_processed;
+        } catch (\Exception $e) {
+            $this->throwException($e, __METHOD__);
+        }
     }
 }
--- tests/models/UsersPurchasesModelTests.php
+++ tests/models/UsersPurchasesModelTests.php
@@ -15,6 +15,9 @@ class UsersPurchasesModelTests extends \PHPUnit_Framework_TestCase
     private static $_id_products = 3;
     private static $_id_deliveries = 4;
     private static $_id_payments = 5;
+    private static $_received = 1;
+    private static $_received_date = 1462453595;
+    private static $_processed = 1;
     
     public static function setUpBeforeClass()
     {
@@ -36,6 +39,16 @@ class UsersPurchasesModelTests extends \PHPUnit_Framework_TestCase
         $this->assertTrue(property_exists($model, 'id_products'));
         $this->assertTrue(property_exists($model, 'id_deliveries'));
         $this->assertTrue(property_exists($model, 'id_payments'));
+        $this->assertTrue(property_exists($model, '_received'));
+        $this->assertTrue(property_exists($model, '_received_date'));
+        $this->assertTrue(property_exists($model, '_processed'));
+        
+        $this->assertTrue(method_exists($model, 'setReceived'));
+        $this->assertTrue(method_exists($model, 'getReceived'));
+        $this->assertTrue(method_exists($model, 'setReceived_date'));
+        $this->assertTrue(method_exists($model, 'getReceived_date'));
+        $this->assertTrue(method_exists($model, 'setProcessed'));
+        $this->assertTrue(method_exists($model, 'getProcessed'));
     }
     
     /**
@@ -58,18 +71,107 @@ class UsersPurchasesModelTests extends \PHPUnit_Framework_TestCase
         $this->assertEquals(self::$_id_payments, $model->id_payments);
         
         $model = new UsersPurchasesModel(['scenario'=>UsersPurchasesModel::GET_FROM_DB]);
-        $model->attributes = ['id'=>self::$_id, 'id_users'=>self::$_id_users, 'id_products'=>self::$_id_products, 'id_deliveries'=>self::$_id_deliveries, 'id_payments'=>self::$_id_payments];
+        $model->attributes = ['id'=>self::$_id, 'id_users'=>self::$_id_users, 'id_products'=>self::$_id_products, 'id_deliveries'=>self::$_id_deliveries, 'id_payments'=>self::$_id_payments, 'received'=>self::$_received, 'received_date'=>self::$_received_date];
         
         $this->assertFalse(empty($model->id));
         $this->assertFalse(empty($model->id_users));
         $this->assertFalse(empty($model->id_products));
         $this->assertFalse(empty($model->id_deliveries));
         $this->assertFalse(empty($model->id_payments));
+        $this->assertFalse(empty($model->received));
+        $this->assertFalse(empty($model->received_date));
         
         $this->assertEquals(self::$_id, $model->id);
         $this->assertEquals(self::$_id_users, $model->id_users);
         $this->assertEquals(self::$_id_products, $model->id_products);
         $this->assertEquals(self::$_id_deliveries, $model->id_deliveries);
         $this->assertEquals(self::$_id_payments, $model->id_payments);
+        $this->assertEquals(self::$_received, $model->received);
+        $this->assertEquals(self::$_received_date, $model->received_date);
+    }
+    
+    /**
+     * Тестирует метод UsersPurchasesModel::setReceived
+     */
+    public function testSetReceived()
+    {
+        $model = new UsersPurchasesModel();
+        $model->received = self::$_received;
+        
+        $this->assertEquals(self::$_received, $model->received);
+    }
+    
+    /**
+     * Тестирует метод UsersPurchasesModel::getReceived
+     */
+    public function testGetReceived()
+    {
+        $model = new UsersPurchasesModel();
+        
+        $this->assertEquals(0, $model->received);
+    }
+    
+    /**
+     * Тестирует метод UsersPurchasesModel::getReceived
+     * в сценарии GET_FROM_FORM
+     */
+    public function testGetReceivedTwo()
+    {
+        $model = new UsersPurchasesModel(['scenario'=>UsersPurchasesModel::GET_FROM_FORM]);
+        
+        $this->assertEquals(self::$_received, $model->received);
+    }
+    
+    /**
+     * Тестирует метод UsersPurchasesModel::setReceived_date
+     */
+    public function testSetReceived_date()
+    {
+        $model = new UsersPurchasesModel();
+        $model->received_date = self::$_received_date;
+        
+        $this->assertEquals(self::$_received_date, $model->received_date);
+    }
+    
+    /**
+     * Тестирует метод UsersPurchasesModel::getReceived_date
+     */
+    public function testGetReceived_date()
+    {
+        $model = new UsersPurchasesModel();
+        
+        $this->assertEquals(NULL, $model->received_date);
+    }
+    
+    /**
+     * Тестирует метод UsersPurchasesModel::getReceived_date
+     * в сценарии GET_FROM_FORM
+     */
+    public function testGetReceived_dateTwo()
+    {
+        $model = new UsersPurchasesModel(['scenario'=>UsersPurchasesModel::GET_FROM_FORM]);
+        
+        $this->assertNotEquals(NULL, $model->received_date);
+    }
+    
+    /**
+     * Тестирует метод UsersPurchasesModel::setProcessed
+     */
+    public function testSetProcessed()
+    {
+        $model = new UsersPurchasesModel();
+        $model->processed = self::$_processed;
+        
+        $this->assertEquals(self::$_processed, $model->processed);
+    }
+    
+    /**
+     * Тестирует метод UsersPurchasesModel::getProcessed
+     */
+    public function testGetProcessed()
+    {
+        $model = new UsersPurchasesModel();
+        
+        $this->assertEquals(0, $model->processed);
     }
 }
