e316cf6d6f4df56b167f482c4b541040aa1a367c
Timofey <timofeysalmin@gmail.com>
2016-07-05 14:18:24 +0300

    050616-0-14:18

--- tests/helpers/MailHelperTests.php
+++ tests/helpers/MailHelperTests.php
@@ -13,9 +13,22 @@ class MailHelperTests extends \PHPUnit_Framework_TestCase
     
     private static $_template = '@app/tests/source/mail/test.twig';
     private static $_setFrom = ['john@somedomain.com'=>'John Dow'];
-    private static $_setTo = ['manager@somedomain.info'=>'Clarice Starling'];
+    private static $_setTo = ['starling@somedomain.info'=>'Clarice Starling'];
     private static $_setSubject = 'Hello, how are you?';
-    private static $_dataForTemplate = ['data'=>'<p>Some data about tomorrow meeting</p>', 'header'=>'Hello, friends!'];
+    private static $_dataForTemplate = ['data'=>'Some data about tomorrow meeting! Please, confirm receipt!', 'header'=>'Hello, friends!'];
+    private static $_contentType = 'text/html; charset=utf-8';
+    
+    public static function setUpBeforeClass()
+    {
+        if (file_exists(\Yii::getAlias(self::$_saveDir))) {
+            $files = scandir(\Yii::getAlias(self::$_saveDir));
+            foreach ($files as $file) {
+                if (strpos($file, '.eml')) {
+                    unlink(\Yii::getAlias(self::$_saveDir . '/' . $file));
+                }
+            }
+        }
+    }
     
     /**
      * Тестирует метод MailHelper::send
@@ -46,11 +59,12 @@ class MailHelperTests extends \PHPUnit_Framework_TestCase
         
         $file = file_get_contents($emlFiles[0]);
         
-        $this->assertTrue((boolean)strpos($file, 'Subject: ' . self::$_setSubject));
-        $this->assertTrue((boolean)strpos($file, 'From: ' . array_values(self::$_setFrom)[0] . ' <' . array_keys(self::$_setFrom)[0] . '>'));
-        $this->assertTrue((boolean)strpos($file, 'To: ' . array_values(self::$_setTo)[0]. ' <' . array_keys(self::$_setTo)[0] . '>'));
-        $this->assertTrue((boolean)strpos($file, self::$_dataForTemplate['data']));
-        $this->assertTrue((boolean)strpos($file, '<h1>' . self::$_dataForTemplate['header'] . '</h1>'));
+        $this->assertEquals(1, preg_match('#' . 'Subject:\s' . self::$_setSubject . '#', $file));
+        $this->assertEquals(1, preg_match('#' . 'From:\s' . array_values(self::$_setFrom)[0] . '\s<' . array_keys(self::$_setFrom)[0] . '>#', $file));
+        $this->assertEquals(1, preg_match('#' . 'To:\s' . array_values(self::$_setTo)[0] . '\s<' . array_keys(self::$_setTo)[0] . '>#', $file));
+        $this->assertEquals(1, preg_match('#Content-Type:\s' . self::$_contentType . '#', $file));
+        $this->assertEquals(1, preg_match('#<h1>' . substr(self::$_dataForTemplate['header'], 0, 10) . '#', $file));
+        $this->assertEquals(1, preg_match('#<p>' . substr(self::$_dataForTemplate['data'], 0, 10) . '#', $file));
     }
     
     public static function tearDownAfterClass()
